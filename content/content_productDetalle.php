68324ed3556510adbb40c26c51ed150f8082621c
Johnny Martinez <johnny@skywebplus.net>
2015-02-15 21:22:32 -0600

    agregue letras al logo

---
+++ content/content_productDetalle.php
@@ -0,0 +1,219 @@
+<div class="pi-section-w pi-section-white">
+	<div class="pi-section pi-section-p pi-clearfix">
+ 
+
+		<div class="pi-layout pi-layout-fluid-fixed">
+			<div class="pi-page-wrapper">
+				<div class="pi-page">
+					<div class="pi-row">
+                        <div class="pi-col-md-12">
+                            <div class="pi-col-md-11"> 
+	                	    <h3 class="pi-uppercase pi-weight-700 pi-letter-spacing pi-has-bg pi-margin-bottom-15">
+	                	    	PC Aple
+	                	    </h3>
+                            </div>
+                            <div class="pi-col-md-1 pi-no-padding"> 
+                                 <h5 class="pi-right pi-no-padding pi-no-margin"> 100 Visitas</h5>
+                            </div>
+                        </div>
+                    </div> 
+                    <!-- - - - - - - - - - SECTION - - - - - - - - - -->
+                    <div class="pi-section-white piCaptions">
+                    	<div class="pi-section pi-no-padding pi-padding-15">
+                    		<div class="pi-row">
+                    	       
+                    	        <div class="pi-col-md-12 pi-no-padding">
+                                    <div class="pi-col-md-1 pi-padding-10">
+                                        <ul class="pi-social-icons pi-jump pi-colored-bg pi-round">
+                                            <li><a href="wwww.facebook.com/Direxionat" class="pi-social-icon-facebook"><i class="icon-facebook"></i></a></li>
+                                            <li class="pi-margin-top-20"><a href="www.twitter.com/Direxionate" class="pi-social-icon-twitter"><i class="icon-twitter"></i></a></li>
+                                            <li class="pi-margin-top-20"><a href="www.plus.google.direxionate.com" class="pi-social-icon-plus"><i class="icon-plus"></i></a></li>
+                                            <li class="pi-margin-top-20"><a href="www.youtube.com/user/direxionat" class="pi-social-icon-youtube"><i class="icon-youtube"></i></a></li>
+                                        </ul>
+
+                                    </div>
+                                    <div class="pi-col-md-5 pi-padding-10">
+                                        <a href="#">
+	                			        	<img style="height:|; border-radius: 10px;" src="img_external/gallery/bg_macbook.png" alt=""/>
+	                			        </a>
+	                			        <div class="wrap">
+			                                <div class="frame" id="basic" style="overflow: hidden;">
+			                                    <ul class="clearfix pi-no-padding" style="list-style-type: none; transform: translateZ(0px) translateX(0px); width: 7068px;">
+			                                    	<li style="width: 112px;">
+			                                            <!-- img-->
+			                                            	<div style="width: 100%;">
+			                                            		<a href="#"><img src="img_external/gallery/bread-and-spikes.jpg" alt=""></a>
+			                                            	</div>
+			                                            <!-- End img -->
+			                                        </li>
+			                                        <li style="width: 115px;">
+			                                            <!-- img-->
+			                                            	<div style="width: 100%;">
+			                                            		<a href="#"><img src="img_external/gallery/bread-and-spikes.jpg" alt=""></a>
+			                                            	</div>
+			                                            <!-- End img -->
+			                                        </li>
+			                                        <li style="width: 115px;">
+			                                            <!-- img-->
+			                                            	<div style="width: 100%;">
+			                                            		<a href="#"><img src="img_external/gallery/bread-and-spikes.jpg" alt=""></a>
+			                                            	</div>
+			                                            <!-- End img -->
+			                                        </li>
+			                                        <li style="width: 115px;">
+			                                            <!-- img-->
+			                                            	<div style="width: 100%;">
+			                                            		<a href="#"><img src="img_external/gallery/bread-and-spikes.jpg" alt=""></a>
+			                                            	</div>
+			                                            <!-- End img -->
+			                                        </li>
+			                                        <li style="width: 115px;">
+			                                            <!-- img-->
+			                                            	<div style="width: 100%;">
+			                                            		<a href="#"><img src="img_external/gallery/bread-and-spikes.jpg" alt=""></a>
+			                                            	</div>
+			                                            <!-- End img -->
+			                                        </li>
+			                                        <li style="width: 115px;">
+			                                            <!-- img-->
+			                                            	<div style="width: 100%;">
+			                                            		<a href="#"><img src="img_external/gallery/bread-and-spikes.jpg" alt=""></a>
+			                                            	</div>
+			                                            <!-- End img -->
+			                                        </li>
+			                                        
+			                                    </ul>
+			                                </div>
+
+			                                <div class="controls center">
+			                                	<button class="btn_left prevPage disabled" disabled="" style="top: 0;"><i class="icon-left-open"></i><i class="icon-chevron-left"></i></button>
+			                                	<button class="btn_rigth nextPage" style="top: 0;"><i class="icon-chevron-right"></i><i class="icon-right-open"></i></button>
+			                                </div>
+		                                </div>
+		                                
+                    	            </div>
+                    	            <div class="pi-col-md-6 pi-padding-10">
+                    	            	<h3 class=""style="float: left;">
+	                	                	$1500.00
+	                	                </h3>
+	                	                <script>
+                                            $(function() {
+                                              $.fn.raty.defaults.path = 'lib/images';
+                                              $('#default').raty({ score: 4 });
+                                            });
+                                        </script> 
+                                        <div id="default" style="float: left; margin-left: 10px; margin-right: 10px;"></div> 
+                                        <br><br>
+                                        <p class="clearfix">
+                                          Somos los mejores en Servicios de peluquería, corte y secado  para mujeres y caballeros tambien. Realizamos extensiones de cabello, manicure y pedicure, mantenimiento, sistema en gel, esculpido, acrigel y pincelado. Tambien contamos con servicio de Barbería. Somos Distribuidores exclusivos de productos Lissi Natural y realizamos su aplicación...                           
+                                        </p>
+                                        <ul class="pi-meta">
+                                        	<li><i class="icon-right-open"></i> Nombre del comercio</li><br>
+                                        	<li><i class="icon-right-open"></i> Ciudad > Estado</li><br>
+                                        	<li><i class="icon-right-open"></i> Categoría</li><br>
+                                        	<li><i class="icon-tag"></i> <a href="">Tag-1</a>, <a href="">Tag-2</a></li>
+                                        </ul>
+                                        <a href="#"><button class="btn-blue pi-right pi-no-margin pi-padding-10">Contactar al comercio patrocinador<i class="icon-right-open"></i></button> </a> 
+
+                    	            </div>
+                                </div>
+                    	    </div>
+                        </div>
+                    </div>
+                    <!-- - - - - - - - - - END SECTION - - - - - - - - - -->
+	            </div>
+            </div>
+			<div class="pi-sidebar-wrapper pi-sidebar-wrapper-right pi-padding-10" style="background-color: rgb(0, 109, 204);">
+			    <div class="pi-sidebar pi-padding-top-10">
+	                <div class="pi-sidebar-block pi-padding-bottom-20">
+	                	<h3 class="h6 pi-uppercase pi-weight-700 pi-letter-spacing pi-has-bg pi-margin-bottom-15">
+	                		Enlaces
+	                	</h3>
+	                	<div class="pi-gallery-item">
+	                		<div class="pi-img-w pi-img-round-corners pi-img-shadow-light">
+	                			<a href="#">
+	                				<img style="height: 200px; border-radius: 10px;" src="img_external/gallery/farmacia.png" alt=""/>
+	                			</a>
+	                		</div>
+	                	</div>
+	                    <!-- Search -->
+	                    <div class="pi-sidebar-block pi-padding-bottom-5">
+	                    	<form class="pi-grouped-input pi-pi-search-form-wide">
+	                    		<button type="submit" class="btn pi-btn-base"><i class="icon-search-1"></i></button>
+	                    		<div id="the-basics-farmacia" class="pi-input-inline">
+	                    			<input type="text" class="typeahead form-control pi-input-wide" placeholder="e.g. Busque en una Ciudad" />
+	                    		</div>
+	                    	</form>
+	                    </div>
+	                    <!-- End search -->
+	                </div>
+		
+	                <div class="pi-sidebar-block pi-padding-bottom-20">
+	                	<h3 class="h6 pi-uppercase pi-weight-700 pi-letter-spacing pi-has-bg pi-margin-bottom-15">
+	                	</h3>
+	                	<div class="pi-gallery-item">
+	                		<div class="pi-img-w pi-img-round-corners pi-img-shadow-light">
+	                			<a href="#">
+	                				<img style="height: 200px; border-radius: 10px;" src="img_external/gallery/emergencia.png" alt=""/>
+	                			</a>
+	                		</div>
+	                	</div>
+	                    <!-- Search -->
+	                    <div class="pi-sidebar-block pi-padding-bottom-5">
+	                    	<form class="pi-grouped-input pi-pi-search-form-wide">
+	                    		<button type="submit" class="btn pi-btn-base"><i class="icon-search-1"></i></button>
+	                    		<div id="the-basics" class="pi-input-inline">
+	                    			<input type="text" class="typeahead form-control pi-input-wide" placeholder="e.g. Busque en una Ciudad" />
+	                    		</div>
+	                    	</form>
+	                    </div>
+	                    <!-- End search -->
+	                </div>
+	                <div class="pi-sidebar-block pi-padding-bottom-20">
+	                	<h3 class="h6 pi-uppercase pi-weight-700 pi-letter-spacing pi-has-bg pi-margin-bottom-15">
+	                	</h3>
+	                	<div class="pi-gallery-item">
+	                		<div class="pi-img-w pi-img-round-corners pi-img-shadow-light">
+	                			<a href="#">
+	                				<img style="height: 200px; border-radius: 10px;" src="img_external/gallery/calendario.png" alt=""/>
+	                			</a>
+	                		</div>
+	                	</div>
+	                </div>
+	                <div class="pi-sidebar-block pi-padding-bottom-20">
+	                	
+	                	<h3 class="h6 pi-uppercase pi-weight-700 pi-letter-spacing pi-has-bg pi-margin-bottom-15">
+	                	Nuestros Amigos
+	                	</h3>
+	                	<div class="pi-gallery-item">
+	                		<div class="pi-img-w pi-img-round-corners pi-img-shadow-light">
+	                			<a href="#">
+	                				<img style="height: 200px; border-radius: 10px;" src="img_external/gallery/rumbera.jpg" alt=""/>
+	                			</a>
+	                		</div>
+	                	</div>
+	                	<div class="pi-gallery-item">
+	                		<div class="pi-img-w pi-img-round-corners pi-img-shadow-light">
+	                			<a href="#">
+	                				<img style="height: 200px; border-radius: 10px;" src="img_external/gallery/anunciate.jpg" alt=""/>
+	                			</a>
+	                		</div>
+	                	</div>
+	                </div>
+	                <div class="pi-sidebar-block pi-padding-bottom-20">
+	                	<h3 class="h6 pi-uppercase pi-weight-700 pi-letter-spacing pi-has-bg pi-margin-bottom-15">
+	                	Google
+	                	</h3>
+	                	<div class="pi-gallery-item">
+	                		<div class="pi-img-w pi-img-round-corners pi-img-shadow-light">
+	                			<a href="#">
+	                				<img style="height: 200px; border-radius: 10px;" src="img_external/gallery/google.jpg" alt=""/>
+	                			</a>
+	                		</div>
+	                	</div>
+	                </div>			
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
