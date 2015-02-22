446780fd3475414796951c98a6de4581c5cd15eb
Johnny Martinez <johnny@skywebplus.net>
2015-02-21 18:26:48 -0600

    update detalle de super ofera

--- content/content_productDetalle.php
+++ content/content_productDetalle.php
@@ -35,61 +35,7 @@
                                     <div class="pi-col-md-5 pi-padding-10">
                                         <a href="#">
 	                			        	<img style="height:|; border-radius: 10px;" src="assets/images/gallery/bg_macbook.png" alt=""/>
-	                			        </a>
-	                			        <div class="wrap">
-			                                <div class="frame" id="basic" style="overflow: hidden;">
-			                                    <ul class="clearfix pi-no-padding" style="list-style-type: none; transform: translateZ(0px) translateX(0px); width: 7068px;">
-			                                    	<li style="width: 112px;">
-			                                            <!-- img-->
-			                                            	<div style="width: 100%;">
-			                                            		<a href="#"><img src="assets/images/gallery/bread-and-spikes.jpg" alt=""></a>
-			                                            	</div>
-			                                            <!-- End img -->
-			                                        </li>
-			                                        <li style="width: 115px;">
-			                                            <!-- img-->
-			                                            	<div style="width: 100%;">
-			                                            		<a href="#"><img src="assets/images/gallery/bread-and-spikes.jpg" alt=""></a>
-			                                            	</div>
-			                                            <!-- End img -->
-			                                        </li>
-			                                        <li style="width: 115px;">
-			                                            <!-- img-->
-			                                            	<div style="width: 100%;">
-			                                            		<a href="#"><img src="assets/images/gallery/bread-and-spikes.jpg" alt=""></a>
-			                                            	</div>
-			                                            <!-- End img -->
-			                                        </li>
-			                                        <li style="width: 115px;">
-			                                            <!-- img-->
-			                                            	<div style="width: 100%;">
-			                                            		<a href="#"><img src="assets/images/gallery/bread-and-spikes.jpg" alt=""></a>
-			                                            	</div>
-			                                            <!-- End img -->
-			                                        </li>
-			                                        <li style="width: 115px;">
-			                                            <!-- img-->
-			                                            	<div style="width: 100%;">
-			                                            		<a href="#"><img src="assets/images/gallery/bread-and-spikes.jpg" alt=""></a>
-			                                            	</div>
-			                                            <!-- End img -->
-			                                        </li>
-			                                        <li style="width: 115px;">
-			                                            <!-- img-->
-			                                            	<div style="width: 100%;">
-			                                            		<a href="#"><img src="assets/images/gallery/bread-and-spikes.jpg" alt=""></a>
-			                                            	</div>
-			                                            <!-- End img -->
-			                                        </li>
-			                                    </ul>
-			                                </div>
-
-			                                <div class="controls center">
-			                                	<button class="btn_left prevPage disabled" disabled="" style="top: 0;"><i class="icon-left-open"></i><i class="icon-chevron-left"></i></button>
-			                                	<button class="btn_rigth nextPage" style="top: 0;"><i class="icon-chevron-right"></i><i class="icon-right-open"></i></button>
-			                                </div>
-		                                </div>
-		                                
+	                			        </a>	                                
                     	            </div>
                     	            <div class="pi-col-md-6 pi-padding-10">
                     	            	<h3 class=""style="float: left;">
@@ -154,54 +100,95 @@
                     	    		Mis otros productos
                     	    	</h3>
 			                    <!-- - - - - - - - - - SECTION - - - - - - - - - -->
-                    <div class="pi-section-white piCaptions">
-                    	<div class="pi-section pi-no-padding pi-padding-15">
-                    		<div class="pi-row" style="border: 1px solid #515291; background-color: white; color: orange;">
-                    	        <div class="pi-no-padding pi-no-margin" style="background: url(assets/images/bg/bg-title.png) #515291 repeat-x top left;">
-                                    <h3 class="pi-uppercase pi-padding-5 pi-text-left" style="font-size: 150%;">
-	                	            	<a href="product-detalle.php">PC Apple</a>
-	                	            </h3>
-                    	        </div>
-                    	        <div class="pi-col-md-12 pi-no-padding">
-                                    <div class="pi-col-md-4 pi-padding-10">
-                                        <a href="product-detalle.php">
-	                			        	<img style="height: 200px; border-radius: 10px;" src="assets/images/gallery/bg_macbook.png" alt=""/>
-	                			        </a>
-	                			        <h3  class="pi-color-orange pi-uppercase pi-no-padding pi-center" style="font-size: 150%;">
-	                	            	   $1500.00
-	                	                </h3> 
-                    	            </div>
-                    	            <div class="pi-col-md-8 pi-padding-10">
-                                        <p class="pi-text-left">
-                                          Somos los mejores en Servicios de peluquería, corte y secado  para mujeres y caballeros tambien. Realizamos extensiones de cabello, manicure y pedicure, mantenimiento, sistema en gel, esculpido, acrigel y pincelado. Tambien contamos con servicio de Barbería. Somos Distribuidores exclusivos de productos Lissi Natural y realizamos su aplicación...                           
-                                        </p>
-                                        <ul class="pi-meta pi-text-left">
-                                        	<li  class="pi-color-orange"><i class="icon-right-open"></i> Nombre del comercio</li><br>
-                                        	<li  class="pi-color-orange"><i class="icon-right-open"></i> Ciudad > Estado</li><br>
-                                        	<li  class="pi-color-orange"><i class="icon-right-open"></i> 100 visitas</li><br>
-                                        	<li  class="pi-color-orange"><i class="icon-right-open"></i> Categoría</li><br>
-                                        	<li  class="pi-color-orange"><i class="icon-tag"></i> <a href=""  class="pi-color-orange">Tag-1</a>, <a href=""  class="pi-color-orange">Tag-2</a></li>
-                                        </ul>
-                                        <a href="#"><button class="btn-blue pi-right pi-no-margin pi-padding-10">Mas Información<i class="icon-right-open"></i></button> </a> 
-                    	            </div>
+                                <div class="pi-section-white piCaptions">
+                                	<div class="pi-section pi-no-padding pi-padding-15">
+                                		<div class="pi-row" style="border: 1px solid #515291; background-color: white; color: orange;">
+                                	        <div class="pi-no-padding pi-no-margin" style="background: url(assets/images/bg/bg-title.png) #515291 repeat-x top left;">
+                                                <h3 class="pi-uppercase pi-padding-5 pi-text-left" style="font-size: 150%;">
+	                            	            	<a href="product-detalle.php">PC Apple</a>
+	                            	            </h3>
+                                	        </div>
+                                	        <div class="pi-col-md-12 pi-no-padding">
+                                                <div class="pi-col-md-4 pi-padding-10">
+                                                    <a href="product-detalle.php">
+	                            			        	<img style="height: 200px; border-radius: 10px;" src="assets/images/gallery/bg_macbook.png" alt=""/>
+	                            			        </a>
+	                            			        <h3  class="pi-color-orange pi-uppercase pi-no-padding pi-center" style="font-size: 150%;">
+	                            	            	   $1500.00
+	                            	                </h3> 
+                                	            </div>
+                                	            <div class="pi-col-md-8 pi-padding-10">
+                                                    <p class="pi-text-left">
+                                                      Somos los mejores en Servicios de peluquería, corte y secado  para mujeres y caballeros tambien. Realizamos extensiones de cabello, manicure y pedicure, mantenimiento, sistema en gel, esculpido, acrigel y pincelado. Tambien contamos con servicio de Barbería. Somos Distribuidores exclusivos de productos Lissi Natural y realizamos su aplicación...                           
+                                                    </p>
+                                                    <ul class="pi-meta pi-text-left">
+                                                    	<li  class="pi-color-orange"><i class="icon-right-open"></i> Nombre del comercio</li><br>
+                                                    	<li  class="pi-color-orange"><i class="icon-right-open"></i> Ciudad > Estado</li><br>
+                                                    	<li  class="pi-color-orange"><i class="icon-right-open"></i> 100 visitas</li><br>
+                                                    	<li  class="pi-color-orange"><i class="icon-right-open"></i> Categoría</li><br>
+                                                    	<li  class="pi-color-orange"><i class="icon-tag"></i> <a href=""  class="pi-color-orange">Tag-1</a>, <a href=""  class="pi-color-orange">Tag-2</a></li>
+                                                    </ul>
+                                                    <a href="#"><button class="btn-blue pi-right pi-no-margin pi-padding-10">Mas Información<i class="icon-right-open"></i></button> </a> 
+                                	            </div>
+                                            </div>
+                                	    </div>
+                                	    <div class="pi-row">
+                                            <div class="pi-col-md-12" style="background: #515291">
+                                                <ul class="pi-meta">
+                                                	<li><i class="icon-facebook"></i><a href="#">Recomendar en Facebook</a></li>
+                                                	<li><i class="icon-twitter"></i><a href="#">Recomendar en Twitter</a></li>
+                                                	<li><i class="icon-plus"></i><a href="#">Recomendar en Google Plus</a></li>
+                                                </ul>
+                                	        </div>
+                                	    </div>
+                                    </div>
                                 </div>
+                                <!-- - - - - - - - - - END SECTION - - - - - - - - - -->  
                     	    </div>
-                    	    <div class="pi-row">
-                                <div class="pi-col-md-12" style="background: #515291">
-                                    <ul class="pi-meta">
-                                    	<li><i class="icon-facebook"></i><a href="#">Recomendar en Facebook</a></li>
-                                    	<li><i class="icon-twitter"></i><a href="#">Recomendar en Twitter</a></li>
-                                    	<li><i class="icon-plus"></i><a href="#">Recomendar en Google Plus</a></li>
+                    	    <h3 class="pi-uppercase pi-weight-500 pi-letter-spacing pi-has-bg pi-margin-bottom-15">
+                                Super Ofertas Recomendadas
+                            </h3>
+                            <div class="wrap">
+                                <div class="frame" id="basic" style="overflow: hidden;">
+                                    <ul class="clearfix pi-no-padding" style="transform: translateZ(0px) translateX(0px); width: 7068px;">
+                                        <li style="background-color: rgba(255, 0, 0, 0.13); padding-top: 10px;">
+                                            <!-- Portfolio item -->
+                                            <div >
+                                                <div class="">
+                                                    <div class="pi-img-w pi-img-shadow pi-img-with-overlay pi-no-margin-bottom">
+                                                        <a href="#"><img src="assets/images/gallery/bg_macbook.png" alt=""></a>
+                                                        <div class="pi-img-overlay pi-img-overlay-darker pi-no-padding">
+                                                            <a href="#"><img src="assets/images/gallery/3.jpg" alt=""></a>
+                                                        </div>
+                                                    </div>
+                                                    <div style="margin-top: 10px; margin-left: 10px;">
+                                                        <h5 class="pi-weight-700 pi-uppercase pi-letter-spacing pi-margin-top-minus-5 pi-text-shadow">
+                                                            <a href="#" class="pi-link-white">Nombre del Producto</a>
+                                                        </h5>
+                                                        <p class="pi-margin-top-minus-10 pi-no-margin"><a href="#" class="pi-link-white ">Nombre del comercio</a></p>
+                                                        <p class="pi-margin-top-minus-5 pi-no-margin">Ciudad/Estado </p>
+                                                        <h5 class="pi-weight-700 pi-uppercasepi-margin-top-10 pi-letter-spacing pi-text-shadow">
+                                                            $ 1500.00
+                                                        </h5>
+                                                        <ul class="pi-caption-links ">
+                                                            <li><i class="icon-tag"></i><a href="#">APPLE</a>, <a href="#">Lapto</a></li>
+                                                        </ul>
+                                                        <a href="product.php" class="btn pi-btn-small pi-btn-base pi-uppercase pi-weight-600 pi-letter-spacing pi-right pi-margin-bottom-10 pi-margin-right-10" style="margin-right: 10px;">Ver Mas...</a>
+                                                    </div>
+                                                </div>
+                                                <div class="pi-img-shadow-gap pi-shadow-effect8"></div>
+                                            </div>
+                                            <!-- End portfolio item -->
+                                        </li>
+
+
                                     </ul>
-                    	        </div>
-                    	    </div>
-                        </div>
-                    </div>
-                    <!-- - - - - - - - - - END SECTION - - - - - - - - - -->
-			                    
-			                    
-                    	    </div>
-                    	    
+                                </div>
+                                <div class="controls center">
+                                    <button class="btn_left prevPage disabled" disabled="" style="top: 37px;"><i class="icon-left-open"></i><i class="icon-chevron-left"></i></button>
+                                    <button class="btn_rigth nextPage" style="top: 37px;"><i class="icon-chevron-right"></i><i class="icon-right-open"></i></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <!-- - - - - - - - - - END SECTION - - - - - - - - - -->
